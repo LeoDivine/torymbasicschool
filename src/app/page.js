ed8620e2e419a6878220577a03482149639ef3d3
LeoDivine <divineonyi2004@gmail.com>
2023-08-04 06:22:53 +0100

    Commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+import LangingPage from "../components/divine/landingpagecomp.jsx";
+
+export default function page() {
+  return (
+    <div>
+      <LangingPage />
+    </div>
+  );
+}
---
+++ src/components/divine/aboutSect.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "./sectComp";
+
+export default function aboutSect() {
+  return (
+    <divs
+      className="my-[30px] h-screen px-[20px] text-center"
+    >
+      <h1 className=" text-[30px] pt-6 font-bold md:text-[40px]">
+        A Perfect Learning Environment for Your Kids
+      </h1>
+      <p className="md:text-[30px]">What We offer</p>
+      <div className="px-[20px] flex flex-col lg:flex-row justify-around flex-wrap md:ml-[70px]">
+        <Card cardTitle="Guidance" cardImage="imgsrc/guidance.jpeg" />
+        <Card  cardTitle="Character Development" cardImage="imgsrc/characterdev.webp"/>
+        <Card cardTitle="Interesting Classroom" cardImage="imgsrc/interestingclass.webp" />
+        <Card cardTitle="Peaceful Environment" cardImage="imgsrc/env.jpg" />
+      </div>
+    </divs>
+  );
+}
